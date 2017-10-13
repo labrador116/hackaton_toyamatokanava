1906f71a75878a4bf777b6a8821cd19ea05e0271
Andrey M <magazon4ik@yandex.ru>
2017-10-13 17:18:55 +0300

    first commit

---
+++ app/src/main/java/com/example/magaz/hackaton_toyamatokanava/MainApplicationActivity.java
@@ -0,0 +1,13 @@
+package com.example.magaz.hackaton_toyamatokanava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainApplicationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_application);
+    }
+}
